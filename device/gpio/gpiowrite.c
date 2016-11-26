7fbebfaf68d2198269866689d375af943d569f78
root <root@debian>
2016-11-26 18:07:40 -0500

    Updated gpiowrite and handler

--- device/gpio/gpiowrite.c
+++ device/gpio/gpiowrite.c
@@ -18,6 +18,7 @@ int32	gpiowrite (
 	gpio1 = (struct gpiod *)GPIO1_BASE_ADDRESS;
 	for(i=0;i<count;i++){
 		data=*(((char *)buf)+i);
+		//kprintf("data %d\t",data);
 		value=data/100;
 		pinnumber=data%100;
 	//	kprintf("data %d val %d pinnum %d\t",data,value,pinnumber);
@@ -27,7 +28,7 @@ int32	gpiowrite (
 		gpio1->gpio_dataout&=~(1<<pinnumber);
 	//	kprintf("after %d\t",gpio1->gpio_dataout);
 		gpio1->gpio_dataout |= value;
-	//	kprintf("gpiowrite value %d\t",gpio1->gpio_dataout);
+	//	kprintf("\ngpiowrite value %d\t",gpio1->gpio_dataout);
 	}
 	//	kprintf("\n");
 	
