b87132377470d2fb7e2d13cba553837f0590d840
Raviteja Ganireddy <ravitejagani@ufl.edu>
2016-11-27 00:47:09 +0000

    updated gpio read

--- device/gpio/gpioread.c
+++ device/gpio/gpioread.c
@@ -5,19 +5,17 @@ extern sid32 read_sem;
 devcall gpioread(struct dentry *devptr, char *buff, int32 count){
 	struct gpiod *gpio,*gpio1;
 	int data;
+	gpio1->gpio_oe |= 1<<count;
 	wait(read_sem);
-	//kprintf("inside read \n");
+	kprintf("inside read \n");
 	gpio1 = (struct gpiod *)GPIO1_BASE_ADDRESS;
 	
 	//read from gpio bank
-	gpio1->gpio_oe &= 0;
 	data = gpio1->gpio_datain;
 	
 	kprintf("gpio 1 value %d \n", data);
-	buff[3] = (data&(0xff000000))>>24;
-	buff[2] = (data&(0x00ff0000))>>16;	
-	buff[1] = (data&(0x0000ff00))>>8;
-	buff[0] = (data&(0x000000ff));
+	data = data&(1<<count);
+	buff[0] = (data>>count);
 
 	//kprintf("gpio irq status %x \n", gpio1->gpio_irqstatus_set_0);
 	//kprintf("gpio rising edge %x \n ", gpio1->gpio_risingdetect);
