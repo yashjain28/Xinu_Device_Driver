efd9eb80dca134869a50b68d372e51806d7733c6
Raviteja Ganireddy <ravitejagani@ufl.edu>
2016-11-27 15:24:29 +0000

    gpioread.c edited

--- device/gpio/gpioread.c
+++ device/gpio/gpioread.c
@@ -5,10 +5,11 @@ extern sid32 read_sem;
 devcall gpioread(struct dentry *devptr, char *buff, int32 count){
 	struct gpiod *gpio,*gpio1;
 	int data;
+	gpio1 = (struct gpiod *)GPIO1_BASE_ADDRESS;
 	gpio1->gpio_oe |= 1<<count;
 	wait(read_sem);
 	kprintf("inside read \n");
-	gpio1 = (struct gpiod *)GPIO1_BASE_ADDRESS;
+	
 	
 	//read from gpio bank
 	data = gpio1->gpio_datain;
